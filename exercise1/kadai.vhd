76f481fbbb343eb390b37d05dba632c5b691e041
jsupratman13 <supratmanjoshua@gmail.com>
2018-10-08 19:40:55 +0900

    fit to fpga board

--- exercise1/kadai.vhd
+++ exercise1/kadai.vhd
@@ -3,8 +3,10 @@ use ieee.std_logic_1164.all;
 
 entity KADAI1 is 
     port (
-        X: in std_logic_vector(2 downto 0 );
-        Y: out std_logic_vector(6 downto 0 );
+        --X: in std_logic_vector(2 downto 0 );
+        --Y: out std_logic_vector(6 downto 0 );
+        PIN_U7, PIN_W9, PIN_M7 : in std_logic;
+        PIN_U21, PIN_V21, PIN_W22, PIN_W21, PIN_Y22, PIN_Y21, PIN_AA22: out std_logic;
 end KADAI1;
 
 architecture RTL of KADAI1 is
@@ -21,18 +23,18 @@ architecture RTL of KADAI1 is
     signal TEMP : std_logic_vector(2 downto 0);
     signal w0 : std_logic;
 begin
-    TEMP(2) <= not X(2);
-    TEMP(1) <= not X(1);
-    TEMP(0) <= not X(0);
+    --TEMP(2) <= not X(2);
+    --TEMP(1) <= not X(1);
+    --TEMP(0) <= not X(0);
+    TEMP(2) <= not PIN_U7;
+    TEMP(1) <= not PIN_W9;
+    TEMP(0) <= not PIN_M7;
 
     i0: HIGH3 port map (
-        TEMP(2)=>A, TEMP(1)=>B, TEMP(2)=>C);
-    i1: HIGH3 port map(
-        Y=>w0 );
+        A=>TEMP(2), B=>TEMP(1), C=>TEMP(0) Y=>w0) );
 
-    i2: DECODER port map (
-        Y=>DATA );
-    i3: DECODER port map (
-        LEDDSP(6)=>Y(6), LEDDSP(5)=>Y(5), LEDDSP(4)=>Y(4), LEDDSP(3)=>Y(3), LEDDSP(2)=>Y(2), LEDDSP(1)=>Y(1), LEDDSP(0)=>Y(0) );
+    i1: DECODER port map (
+        -- DATA=>w0, LEDDSP(6)=>Y(6), LEDDSP(5)=>Y(5), LEDDSP(4)=>Y(4), LEDDSP(3)=>Y(3), LEDDSP(2)=>Y(2), LEDDSP(1)=>Y(1), LEDDSP(0)=>Y(0) );
+        DATA=>w0, LEDDSP(6)=>PIN_AA22, LEDDSP(5)=>PIN_Y21, LEDDSP(4)=>PIN_Y22, LEDDSP(3)=>PIN_W21, LEDDSP(2)=>PIN_W22, LEDDSP(1)=>PIN_V21, LEDDSP(0)=>PIN_U21 );
 
 end RTL;
